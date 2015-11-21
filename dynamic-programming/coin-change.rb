6fd79da820d7c5fdbec90767275ae77b62fce0c4
Robbert Brak <robbert@finalist.com>
2015-11-21 14:33:35 +0100

    Solved the coin change problem

--- dynamic-programming/coin-change.rb
+++ dynamic-programming/coin-change.rb
@@ -1,33 +1,19 @@
 require 'set'
 
 n, m = gets.split.map &:to_i
-coins = gets.split.map &:to_i
+coins = gets.split.map(&:to_i).sort
 
-solutions = Array.new(n)
+solutions = Array.new(m)
 
-def solve(n, solutions, coins)
-  return Set.new if n < 0
-  return solutions[n] unless solutions[n].nil?
-
-  solutions[n] = Set.new
-
-  if n == 0
-    solutions[n].add(Hash.new)
-    return solutions[n]
+m.times do |i|
+  solutions[i] = Array.new(n+1, 0)
+  (n+1).times do |j|
+    solutions[i][j] = 1 if i == 0 && j == 0
+    solutions[i][j] += solutions[i-1][j] if i > 0
+    solutions[i][j] += solutions[i][j - coins[i]] if j - coins[i] >= 0
+    #puts "#{i} #{j} #{coins[i]}"
+    #p solutions
   end
-
-  coins.each do |coin|
-    smaller_solutions = solve(n - coin, solutions, coins)
-    smaller_solutions.each do |solution|
-      solution = solution.clone
-      solution[coin] = (solution[coin] || 0) + 1
-      solutions[n].add(solution)
-    end
-  end
-
-  p solutions[n]
-  solutions[n]
 end
 
-final_solutions = solve(n, solutions, coins)
-puts final_solutions.length
+puts solutions[-1][-1]
